768b3d4a3636464e85d2f3a3f93e71243c25e98f
Andy <andy@mgpstore.com>
2023-03-20 17:15:45 +0700

    update display bagde

--- js/contentScript.js
+++ js/contentScript.js
@@ -4,8 +4,13 @@ chrome.runtime.sendMessage({authorizationData: localStorage.getItem('authorizati
 var div = document.createElement("div");
 
 chrome.runtime.sendMessage({action: "getGPA"}, function(response) {
-    div.innerHTML = ` GPA: <b> ${response}</b>`;
-    div.className = 'gpa-badge'
+    if(response){
+        div.innerHTML = ` GPA: <b> ${response}</b>`;
+        div.className = 'gpa-badge'
+    } else {
+        div.innerHTML = `Please reload tab!!`;
+        div.className = 'gpa-badge'
+    }
 });
 
 document.body.appendChild(div);
